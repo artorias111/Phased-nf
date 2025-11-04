c82ab2520749c4c0ab63956ae0b5c8b4fbb39703
nletien <nletien2@illinois.edu>
2025-11-04 10:15:49 -0600

    shell scripts fix to remove most hard-coding

--- helper-scripts/FCS-adapter-screen-script.sh
+++ helper-scripts/FCS-adapter-screen-script.sh
@@ -1,1 +1,9 @@
-./run_fcsadaptor.sh --fasta-input sample-assembly-practice/analysis/Pbrachy.hic.p_ctg.fa --output-dir ./sample-assembly-practice/analysis/adaptor
+#!/usr/bin/env bash
+
+#set variables and paths
+sequence_file='/data2/nletien2/sample-assembly-practice/Pbrachy.hic.p_ctg.fa'
+
+assembly_name='Pbrachy'
+
+#Do not edit below this line
+./run_fcsadaptor.sh --fasta-input ${sequence_file} --output-dir ${assembly_name}.FCS-adapter-screen --euk |& tee ${assembly_name}.fcs-adapter-screen.log
--- helper-scripts/quast-script.sh
+++ helper-scripts/quast-script.sh
@@ -1,1 +1,11 @@
-quast -o Pbrachy.hic.p_ctg -t 18 Pbrachy.hic.p_ctg.fa
+#!/usr/bin/env bash
+
+#set variables and paths
+sequence_file='/data2/nletien2/sample-assembly-practice/Pbrachy.hic.p_ctg.fa'
+
+assembly_name='Pbrachy'
+num_threads=18
+
+
+#do not edit below this line
+quast -o ${assembly_name}.quast -t ${num_threads} ${sequence_file} |& tee ${assembly_name}.quast.log
